daf8a204bba9724055387338b18ffb85637d17f6
Daniel Namaki <namaki.work@gmail.com>
2024-06-16 15:49:55 +0200

    update project

---
+++ src/main/java/it/unipi/tonystark/App.java
@@ -0,0 +1,13 @@
+package it.unipi.tonystark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
